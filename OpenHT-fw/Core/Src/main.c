5e7b95405a0dd174b25dc4aa78b66a8773e20670
Morgan Diepart <morgandiepart@gmail.com>
2023-08-24 21:07:43 +0200

    Updated CubeMX to 6.9.1

--- OpenHT-fw/Core/Src/main.c
+++ OpenHT-fw/Core/Src/main.c
@@ -350,6 +350,7 @@ int main(void)
 
   /* Start scheduler */
   osKernelStart();
+
   /* We should never get here as control is now taken by the scheduler */
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
